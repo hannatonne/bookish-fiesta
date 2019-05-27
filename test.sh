b2881e4beada6583828b7cdd96ec2e6cb09414d5
Meichsner <jmeichsn@ippjepc025.VK.Medizin.Uni-Mainz.DE>
2019-05-27 11:09:04 +0200

    "changed file by commenting out the last line"

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ echo "Hello $(whoami) !"
 echo "The path you are in is: $PWD"
 nrngui -nogui mosinit.hoc 
 
-cp Parameter.hoc data-seedU77-odorU1-km2p0.4/Parameter.hoc
+# cp Parameter.hoc data-seedU77-odorU1-km2p0.4/Parameter.hoc
